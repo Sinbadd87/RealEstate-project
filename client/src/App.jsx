8e96a3a7876cf5f6cb0a933b59d4011361d71222
Sinbadd87 <dtishen87@gmail.com>
2024-06-01 20:41:53 +0300

    added spinner component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,21 @@
-import { Routes, Route } from "react-router-dom";
-
-import Navigation from "./routes/navigation/Navigation";
-import Footer from "./routes/footer/Footer";
-import Home from "./routes/home/Home";
 import "./App.scss";
-import Contacts from "./routes/contacts/Contacts";
-import ProjectsPage from "./routes/projects/ProjectsPage";
-import ProjectPageCard from "./routes/projects/projectPageCard/ProjectPageCard";
-import Auth from "./routes/authentication/Auth";
-import Reservation from "./routes/reservation/Reservation";
-import ApartmentsPage from "./routes/apartmentsPage/ApartmentsPage";
 
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { useGetAuthUserQuery } from "./api/projectApiSlice";
 import { useDispatch } from "react-redux";
 import { setCredentials } from "./features/auth/authSlice";
+import { Routes, Route } from "react-router-dom";
+import Spinner from "./components/spinner/Spinner";
+
+import ApartmentsPage from "./routes/apartmentsPage/ApartmentsPage";
+import Auth from "./routes/authentication/Auth";
+import Contacts from "./routes/contacts/Contacts";
+import Footer from "./routes/footer/Footer";
+import Home from "./routes/home/Home";
+import Navigation from "./routes/navigation/Navigation";
+import ProjectsPage from "./routes/projects/ProjectsPage";
+import ProjectPageCard from "./routes/projects/projectPageCard/ProjectPageCard";
+import Reservation from "./routes/reservation/Reservation";
 
 function App() {
   const { data } = useGetAuthUserQuery();
@@ -32,29 +33,31 @@ function App() {
   }, [data]);
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <>
-            <Navigation />
-            <Footer />
-          </>
-        }
-      >
-        <Route index element={<Home />} />
-        <Route path="/projects">
-          <Route index element={<ProjectsPage />} />
-          <Route path=":id">
-            <Route index element={<ProjectPageCard />} />
-            <Route path="apartments" element={<ApartmentsPage />} />
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navigation />
+              <Footer />
+            </>
+          }
+        >
+          <Route index element={<Home />} />
+          <Route path="/projects">
+            <Route index element={<ProjectsPage />} />
+            <Route path=":id">
+              <Route index element={<ProjectPageCard />} />
+              <Route path="apartments" element={<ApartmentsPage />} />
+            </Route>
           </Route>
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/reservation" element={<Reservation />} />
         </Route>
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/reservation" element={<Reservation />} />
-      </Route>
-    </Routes>
+      </Routes>
+    </Suspense>
   );
 }
 
---
+++ client/src/components/spinner/Spinner.jsx
@@ -0,0 +1,11 @@
+import "./spinner.scss";
+
+const Spinner = () => {
+  return (
+    <div className="spinnerOverlay">
+      <div className="spinnerContainer"></div>
+    </div>
+  );
+};
+
+export default Spinner;
